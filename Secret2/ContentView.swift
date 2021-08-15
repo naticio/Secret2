adb91327424747b7b6738685b82ed91a876a69a2
Nat-Serrano <naticio@gmail.com>
2021-08-14 20:55:15 -0500

    Initial Commit

---
+++ Secret2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Secret2
+//
+//  Created by Nat-Serrano on 8/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Secret2/Secret2App.swift
@@ -0,0 +1,17 @@
+//
+//  Secret2App.swift
+//  Secret2
+//
+//  Created by Nat-Serrano on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Secret2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
